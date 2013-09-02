e66843798d45cc8777e3a546ec1b7329585d7151
Alan Gardner <urfolomeus@gmail.com>
2013-09-02 22:59:55 +0100

    Renaming dom test
    Signed-off-by: Alan Gardner <urfolomeus@gmail.com>

---
+++ spec/javascripts/dom_spec.js
@@ -0,0 +1,10 @@
+//= require jquery
+//= require views
+
+// describe('DOM test', function() {
+//   it('should render', function() {
+//     appendTo('#konacha');
+//     var result = $('#konacha div').hasClass('hello-world');
+//     expect(result).to.be.ok;
+//   });
+// });
