874d63cbfdf9f17208a64973a2d74088e0f1ad76
Alan Gardner <urfolomeus@gmail.com>
2013-09-02 21:05:57 +0100

    Initial commit
    Signed-off-by: Alan Gardner <urfolomeus@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MochaTest::Application.config.secret_token = '76f3666ed7c9f7fe728b9c75fdbd2d0104afd582fe16a3b466e4985ffb1a5fb13c87307effd66b7eb827c4de77cdf3e201333c7db0604d61f4f41c446d34159f'
